5edda2f4c60df1dec3afb1f146c60437e50bc7e5
Javier <j-a-a1996@hotmail.es>
2021-05-04 08:59:49 +0200

    Cosas

--- app/src/main/java/com/artigas96/calendarlabourhours/ui/home/HomeFragment.java
+++ app/src/main/java/com/artigas96/calendarlabourhours/ui/home/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,6 +14,10 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.artigas96.calendarlabourhours.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class HomeFragment extends Fragment {
 
@@ -23,11 +28,15 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        //final TextView textView = root.findViewById(R.id.text_home);
+
+        CalendarView calendarView = root.findViewById(R.id.calendarMain);
+        FloatingActionButton fab = root.findViewById(R.id.addHours);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                Calendar calendar= Calendar.getInstance();
+                calendar.setTime(new Date(calendarView.getDate()));
             }
         });
         return root;
